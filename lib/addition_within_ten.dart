4ec70639f28983ecfa3c1b73db34ff0916151fde
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2022-03-30 16:40:31 +0800

    Add function to generate two numbers and check answer

--- lib/addition_within_ten.dart
+++ lib/addition_within_ten.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'dart:math';
 
 class AdditionWithinTen extends StatefulWidget {
@@ -9,13 +10,12 @@ class AdditionWithinTen extends StatefulWidget {
 }
 
 class _AdditionWithinTenState extends State<AdditionWithinTen> {
-  final int _num1 = Random().nextInt(10);
-  final int _num2 = Random().nextInt(10);
+  int _num1 = Random().nextInt(10);
+  int _num2 = Random().nextInt(10);
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      // padding: const EdgeInsets.all(32),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
@@ -24,17 +24,45 @@ class _AdditionWithinTenState extends State<AdditionWithinTen> {
               child: Text('$_num1 + $_num2 ='),
             ),
           ),
-          const Expanded(
+          Expanded(
             child: TextField(
               autofocus: true,
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly
+              ],
+              onSubmitted: _checkAnswer,
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
-                hintText: 'Answer is ?',
               ),
             ),
+          ),
+          OutlinedButton(
+            child: Text("Next"),
+            onPressed: _generateNumbers,
           ),
         ],
       ),
     );
+  }
+
+  void _generateNumbers() {
+    setState(() {
+      bool firstTime = true;
+      while (firstTime || _num1 + _num2 > 10) {
+        _num1 = Random().nextInt(10);
+        _num2 = Random().nextInt(10);
+        firstTime = false;
+      }
+    });
+  }
+
+  void _checkAnswer(String answer) {
+    if (int.parse(answer) == _num1 + _num2) {
+      debugPrint('The answer is correct!');
+    } else {
+      debugPrint('Try again.');
+    }
   }
 }
